f909c15fec60a97d29b89f489aed283fa8ba65dc
galmungral <hewenqi96@gmail.com>
2021-11-22 23:48:28 +0800

    fix: dist path

--- release/1.0.0/install-linux.sh
+++ release/1.0.0/install-linux.sh
@@ -1,4 +1,4 @@
-DIST_DIR=https://raw.githubusercontent.com/GalMunGral/telescope/master/1.0.0
+DIST_DIR=https://raw.githubusercontent.com/GalMunGral/telescope/master/release/1.0.0
 
 curl -fsSLo /usr/local/bin/telescope-remote $DIST_DIR/remote-x86_64-linux-gnu
 curl -fsSLo /etc/systemd/system/telescope.service $DIST_DIR/telescope.service
