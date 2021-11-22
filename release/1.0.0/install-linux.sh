63f4561fcf5a7112416a4629c572ae957e6fe86a
galmungral <hewenqi96@gmail.com>
2021-11-23 00:26:32 +0800

    deploy: scripts

--- release/1.0.0/install-linux.sh
+++ release/1.0.0/install-linux.sh
@@ -1,7 +1,7 @@
 DIST_DIR=https://raw.githubusercontent.com/GalMunGral/telescope/master/release/1.0.0
 
-curl -fsSLo /usr/local/bin/telescope-remote $DIST_DIR/remote-x86_64-linux-gnu
-curl -fsSLo /etc/systemd/system/telescope.service $DIST_DIR/telescope.service
+curl -SLo /usr/local/bin/telescope-remote $DIST_DIR/remote-x86_64-linux-gnu
+curl -SLo /etc/systemd/system/telescope.service $DIST_DIR/telescope.service
 
 chmod 744 /usr/local/bin/telescope-remote
 
--- release/1.0.0/install-macos.sh
+++ release/1.0.0/install-macos.sh
@@ -1,8 +1,25 @@
 DIST_DIR=https://raw.githubusercontent.com/GalMunGral/telescope/master/release/1.0.0
 
-curl -fsSLo /usr/local/bin/telescope-local $DIST_DIR/local-arm64-apple-darwin20.3.0
-curl -fsSLo /Library/LaunchDaemons/telescope.plist $DIST_DIR/telescope.plist
+# this doesn't seem to work behind a proxy, hmmm...
+# curl -SLo /usr/local/bin/telescope-local $DIST_DIR/local-arm64-apple-darwin20.3.0
+# curl -SLo /Library/LaunchDaemons/telescope.plist $DIST_DIR/telescope.plist
 
 launchctl load /Library/LaunchDaemons/telescope.plist
 launchctl start com.galmungral.telescope
 launchctl list | grep telescope
+
+PROXY=socks5h://127.0.0.1:3030
+
+echo "
+# telescope-local
+
+function t-on {
+  export {HTTP{,S},ALL}_PROXY=$PROXY
+  export {http{,s},all}_proxy=$PROXY
+}
+
+function t-off {
+  unset {HTTP{,S},ALL}_PROXY
+  unset {http{,s},all}_proxy
+}
+" >> ~/.zshrc
