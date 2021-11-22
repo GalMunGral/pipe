5984d0d04cff3bbcf99a8c1fc14610c83aac1b03
galmungral <hewenqi96@gmail.com>
2021-11-22 23:55:28 +0800

    fix: file mode

--- release/1.0.0/install-linux.sh
+++ release/1.0.0/install-linux.sh
@@ -3,6 +3,8 @@ DIST_DIR=https://raw.githubusercontent.com/GalMunGral/telescope/master/release/1
 curl -fsSLo /usr/local/bin/telescope-remote $DIST_DIR/remote-x86_64-linux-gnu
 curl -fsSLo /etc/systemd/system/telescope.service $DIST_DIR/telescope.service
 
+chmod 744 /usr/local/bin/telescope-remote
+
 systemctl daemon-reload
 systemctl restart telescope
 systemctl status telescope
