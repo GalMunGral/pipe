a0d9008d27485e19e7f7f39504e98630e7134478
Wenqi He <hewenqi96@gmail.com>
2022-01-18 18:11:02 +0800

    build: fix http proxy

--- build/1.1.0/install-macos.sh
+++ build/1.1.0/install-macos.sh
@@ -1,15 +1,14 @@
+sudo mkdir -p /usr/local/bin
 sudo cp ./local-arm64-apple-darwin20.3.0 /usr/local/bin/telescope-local
-sudo cp ./telescope-local.plist /Library/LaunchAgents/telescope-local.plist 
-
 sudo cp ./http-arm64-apple-darwin20.3.0 /usr/local/bin/telescope-http
-sudo cp ./telescope-http.plist /Library/LaunchAgents/telescope-http.plist 
 
+sudo cp ./telescope-local.plist /Library/LaunchAgents/telescope-local.plist 
+sudo cp ./telescope-http.plist /Library/LaunchAgents/telescope-http.plist 
 sudo chown root:wheel /Library/LaunchAgents/telescope-*.plist
 sudo chmod o-w /Library/LaunchAgents/telescope-*.plist
 
 launchctl load /Library/LaunchAgents/telescope-local.plist
 launchctl start com.galmungral.telescope-local
-
 launchctl load /Library/LaunchAgents/telescope-http.plist
 launchctl start com.galmungral.telescope-http
 
